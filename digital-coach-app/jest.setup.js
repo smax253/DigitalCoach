9e34cff9cb655333c9ea9f675eda64484696ae02
Ming Lin <minglianglin50@gmail.com>
2021-12-26 16:55:35 -0500

    fix firebase emulator setup with testing

--- digital-coach-app/jest.setup.js
+++ digital-coach-app/jest.setup.js
@@ -1,4 +1,33 @@
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import { getStorage, connectStorageEmulator } from "firebase/storage";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp, getApps } from "firebase/app";
+
+import { firebaseConfig } from "@App/lib/firebase/firebase.config";
 
 configure({ adapter: new Adapter() });
+
+const localIp = "localhost";
+
+if (!getApps.length) {
+  const app = initializeApp(firebaseConfig);
+  if (typeof window !== "undefined") {
+    if ("measurementId" in firebaseConfig) {
+      getAnalytics();
+    }
+  }
+
+  const auth = getAuth(app);
+  connectAuthEmulator(auth, `http://${localIp}:9099`, {
+    disableWarnings: true,
+  });
+
+  const db = getFirestore(app);
+  connectFirestoreEmulator(db, localIp, 8080);
+
+  const storage = getStorage(app);
+  connectStorageEmulator(storage, localIp, 9199);
+}
---
+++ digital-coach-app/specs/jestGlobalSetup.ts
@@ -0,0 +1,3 @@
+export default function () {
+  require("dotenv-flow").config();
+}
