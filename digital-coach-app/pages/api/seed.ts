f87285f28059d196ec2749155b75c7d82c57e7bf
Ming Lin <minglianglin50@gmail.com>
2021-12-26 17:04:35 -0500

    okay last clean up im done

--- digital-coach-app/pages/api/seed.ts
+++ digital-coach-app/pages/api/seed.ts
@@ -9,28 +9,32 @@ export default async function seed(
   req: NextApiRequest,
   res: NextApiResponse<{}>
 ) {
-  const userCredentials = await Promise.all([
-    AuthService.signup("ming@test.com", "password"),
-    AuthService.signup("appu@test.com", "password"),
-    AuthService.signup("suzy@test.com", "password"),
-    AuthService.signup("mike@test.com", "password"),
-    AuthService.signup("max@test.com", "password"),
-    AuthService.signup("hamzah@test.com", "password"),
-  ]);
+  try {
+    const userCredentials = await Promise.all([
+      AuthService.signup("ming@test.com", "password"),
+      AuthService.signup("appu@test.com", "password"),
+      AuthService.signup("suzy@test.com", "password"),
+      AuthService.signup("mike@test.com", "password"),
+      AuthService.signup("max@test.com", "password"),
+      AuthService.signup("hamzah@test.com", "password"),
+    ]);
 
-  await Promise.all(
-    userCredentials.map(({ user }, idx) =>
-      UserService.add(
-        new UserBuilder()
-          .withId(user.uid)
-          .with({
-            email: user.email!,
-            name: user.email?.split("@")[0],
-          })
-          .build()
+    await Promise.all(
+      userCredentials.map(({ user }, idx) =>
+        UserService.add(
+          new UserBuilder()
+            .withId(user.uid)
+            .with({
+              email: user.email!,
+              name: user.email?.split("@")[0],
+            })
+            .build()
+        )
       )
-    )
-  );
+    );
 
-  res.status(200).json({ message: "Finished seeding users" });
+    res.status(200).json({ message: "Finished seeding users" });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 }
