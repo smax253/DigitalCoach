b352d7c40cc4f67e81b07c7f8f7b86dab027bd9c
smax253 <smax253@gmail.com>
2022-02-21 21:43:17 -0500

    Login and register page styling
    Former-commit-id: 9c59b759bff628cb450deea733e093a12cd264e9

--- digital-coach-app/pages/auth/signup.tsx
+++ digital-coach-app/pages/auth/signup.tsx
@@ -1,12 +1,15 @@
 import { useForm } from "react-hook-form";
 import Button from "@App/components/atoms/Button";
 import useAuthContext from "@App/lib/auth/AuthContext";
-import styles from "@App/styles/login.module.scss";
+import styles from "@App/styles/LoginPage.module.scss";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { TextField } from "@App/components/TextField";
+import { TextField } from "@App/components/molecules/TextField";
 import Link from "next/link";
 import UnAuthGuard from "@App/lib/auth/UnAuthGuard";
+import CenteredComponent from "@App/components/atoms/CenteredComponent";
+import HowToRegIcon from '@mui/icons-material/HowToReg';
+
 
 interface LoginFormInputs {
   email: string;
@@ -46,38 +49,52 @@ export default function SignUpPage() {
 
   return (
     <UnAuthGuard>
-      <h1>Sign up</h1>
-      <h1>{currentUser?.id}</h1>
+      <CenteredComponent>
+        <div className={styles.loginBox}>
+         
+          <h1>{currentUser?.id}</h1>
 
-      {authError && <p>{authError}</p>}
-      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-        <TextField type="email" placeholder="Email" {...register("email")} />
-        {formError.email && <span>{formError.email.message}</span>}
+          {authError && <p>{authError}</p>}
+          <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+            <div className={styles.logo}>
+              <h1>Digital Coach</h1>
+            </div>
+            <h2>Register an Account</h2>
+            <h3>Email</h3>
+            <TextField type="email" placeholder="" {...register("email")} />
+            {formError.email && <span>{formError.email.message}</span>}
+            <h3>Password</h3>
+            <TextField
+              type="password"
+              autoComplete="on"
+              placeholder=""
+              {...register("password")}
+            />
+            {formError.password && <span>{formError.password.message}</span>}
+            <h3>Confirm Password</h3>
 
-        <TextField
-          type="password"
-          autoComplete="on"
-          placeholder="Password"
-          {...register("password")}
-        />
-        {formError.password && <span>{formError.password.message}</span>}
+            <TextField
+              type="password"
+              autoComplete="on"
+              placeholder=""
+              {...register("passwordConfirm")}
+            />
+            {formError.passwordConfirm && (
+              <span>{formError.passwordConfirm.message}</span>
+            )}
 
-        <TextField
-          type="password"
-          autoComplete="on"
-          placeholder="Confirm Password"
-          {...register("passwordConfirm")}
-        />
-        {formError.passwordConfirm && (
-          <span>{formError.passwordConfirm.message}</span>
-        )}
+            <Button type="submit">
+              <HowToRegIcon/>
+              Register
+            </Button>
+            <Link href="/auth/login">
+              <a>Have an account? log in</a>
+            </Link>
+          </form>
 
-        <Button type="submit">sign up</Button>
-      </form>
-
-      <Link href="/auth/login">
-        <a>Have an account? log in</a>
-      </Link>
+          
+        </div>
+      </CenteredComponent>
     </UnAuthGuard>
   );
 }
