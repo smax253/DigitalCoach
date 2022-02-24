a4cb165b85d7cdacbe070561d3c387d29bff4c86
Hamzah Nizami <hnizami1@stevens.edu>
2022-02-24 12:07:57 -0500

    Initial code. Testing it functions

--- ml-api/models/views.py
+++ ml-api/models/views.py
@@ -1,17 +1,79 @@
-import flask 
-import io 
+
 import string
-import time 
-import os 
-import numpy as np
-import tensorflow as tf 
 from flask import Flask, jsonify, request 
+import pickle
+import pandas as pd
+import re, string
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Load the model herer 
-model = 'model'
+MODEL = pickle.load(open('text_model.pkl', 'rb'))
 
 # initalize the Flask object
 app = Flask(__name__)
+
+#convert to lowercase, strip and remove punctuations
+def preprocess(text):
+    text = text.lower() 
+    text=text.strip()  
+    text=re.compile('<.*?>').sub('', text) 
+    text = re.compile('[%s]' % re.escape(string.punctuation)).sub(' ', text)  
+    text = re.sub('\s+', ' ', text)  
+    text = re.sub(r'\[[0-9]*\]',' ',text) 
+    text=re.sub(r'[^\w\s]', '', str(text).lower().strip())
+    text = re.sub(r'\d',' ',text) 
+    text = re.sub(r'\s+',' ',text) 
+    return text
+
+ 
+# STOPWORD REMOVAL
+def stopword(string):
+    a= [i for i in string.split() if i not in stopwords.words('english') and i not in ["uhh", "umm", "uhm"]]
+    return ' '.join(a)
+#LEMMATIZATION
+# Initialize the lemmatizer
+wl = WordNetLemmatizer()
+ 
+# This is a helper function to map NTLK position tags
+def get_wordnet_pos(tag):
+    if tag.startswith('J'):
+        return wordnet.ADJ
+    elif tag.startswith('V'):
+        return wordnet.VERB
+    elif tag.startswith('N'):
+        return wordnet.NOUN
+    elif tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return wordnet.NOUN
+# Tokenize the sentence
+def lemmatizer(string):
+    word_pos_tags = nltk.pos_tag(word_tokenize(string)) # Get position tags
+    a=[wl.lemmatize(tag[0], get_wordnet_pos(tag[1])) for idx, tag in enumerate(word_pos_tags)] # Map the position tag and lemmatize the word/token
+    return " ".join(a)
+
+def finalpreprocess(string):
+    return lemmatizer(stopword(preprocess(string)))
+
+
+def clean_text(answer):
+    process_answer = [[answer]]
+    df = pd.DataFrame(process_answer, columns = ['sentence'])
+    df['cleaned'] = df['sentence'].apply(lambda x: finalpreprocess(x))
+    return df['cleaned']
+
+def predict_text_structure(text):
+    tfidf_vectorizer = TfidfVectorizer(use_idf=True)
+    X_vector = tfidf_vectorizer.transform(text)
+    y_prob = MODEL.predict_proba(X_vector)[:,1][0]
+    return y_prob, 1 if y_prob > 0.5 else 0
+
 
 '''
 POST request with a text option within the text body 
@@ -19,4 +81,18 @@ that has to get processed and then fed into the model.
 '''
 @app.route('/predict-text', method=['POST'])
 def score_text_structure(): 
-    pass
+    if not request.answer: 
+        return 'Text to predict does not exist.'
+    text = request.answer
+    cleaned = clean_text(text)
+    predictions = predict_text_structure(cleaned)
+    return jsonify(percent_prediction=predictions[0],
+                   binary_prediction=predictions[1])
+
+
+@app.route('/', methods=['GET'])
+def index(): 
+    return 'Welcome to the ML API for Digital Coach'
+
+if __name__ == '___main__':
+    app.run(debug=True, host='0.0.0.0')
