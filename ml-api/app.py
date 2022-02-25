f94e87f76ca88661263d868754448b48e9d01fbe
Hamzah Nizami <hnizami1@stevens.edu>
2022-02-24 21:33:48 -0500

    Import FER error

--- ml-api/app.py
+++ ml-api/app.py
@@ -1,3 +1,6 @@
+"""
+Main Flask application as well as routes of the app.
+"""
 from flask import Flask, jsonify, request
 from helpers.text_processor import clean_text
 from helpers.text_predict import predict_text_structure
@@ -5,25 +8,40 @@ from helpers.text_predict import predict_text_structure
 # initalize the Flask object
 app = Flask(__name__)
 
-"""
-POST request with a text option within the text body 
-that has to get processed and then fed into the model.
-"""
-
 
 @app.route("/predict-text", methods=["POST"])
 def score_text_structure():
+    """
+    POST route to score how structured the user's answers are.
+    """
     content = request.get_json()
     if not content["answer"]:
-        return "Text to predict does not exist."
+        return jsonify(errors="Text to predict does not exist.")
     text = content["answer"]
     cleaned = clean_text(text)
     predictions = predict_text_structure(cleaned)
     return jsonify(percent_prediction=predictions[0], binary_prediction=predictions[1])
 
 
+@app.route("/predict-facial", methods=["POST"])
+def score_facial():
+    """
+    POST route to score user's facial expressions.
+    """
+    if "file" not in request.files:
+        return jsonify(errors="No video provided.")
+    video = request.files.get("file")
+    if not video:
+        return jsonify(errors="Error grabbing video from request.")
+    path = "/"
+    
+
+
 @app.route("/", methods=["GET"])
 def index():
+    """
+    Home route.
+    """
     return "Welcome to the ML API for Digital Coach"
 
 
