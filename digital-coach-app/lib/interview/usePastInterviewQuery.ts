e1f845c341a0deea8074b61b159cf9923eab85fc
Max Shi <smax253@gmail.com>
2022-03-22 15:53:01 -0400

    Past Interview Table connect to database (#16)
    * connect past interview table to database
    
    * Fix issues with query hook and console logs
    
    Former-commit-id: 2d90470c24b236f1de3f18bf08a5bad0fd2fdb61

---
+++ digital-coach-app/lib/interview/usePastInterviewQuery.ts
@@ -0,0 +1,37 @@
+import { useQuery } from "react-query";
+import InterviewQuestionService from "../interviewQuestion/InterviewQuestionService";
+import InterviewService from "./InterviewService";
+
+export default function usePastInterviewQuery(userId: string | undefined) {
+  return useQuery(
+    ["pastUserInterviews"],
+    async () => {
+      const interviews = await InterviewService.fetchUserInterviews(userId!);
+      const interviewLookup = interviews.docs.map(async (interview) => {
+        const interviewData =
+          await InterviewQuestionService.getInterviewQuestionFromRefPath(
+            userId!,
+            interview.id
+          );
+        const interviewQuestions = interviewData.docs;
+        const scores = interviewQuestions
+          .map((interview) => interview.data().score)
+          .filter(
+            (score): score is number => score !== undefined && score !== null
+          );
+        const averageScore =
+          scores.reduce((acc, score) => acc + score, 0) / scores.length;
+        const completed = interviewQuestions.map(
+          (interview) => interview.data().answeredAt !== null
+        );
+        const completionPct =
+          completed.filter((item) => item).length / completed.length;
+        return {interviewName: interview.data().title, date: interview.data().createdAt.toDate().toTimeString() ,averageScore, completionPct}
+      });
+      return Promise.all(interviewLookup);
+    },
+    {
+      enabled: !!userId,
+    }
+  );
+}
