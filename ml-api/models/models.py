6d350fb4709a5a3218b39e03466b0f2bc154b4fe
Hamzah Nizami <hnizami1@stevens.edu>
2022-03-30 23:28:14 -0400

    ML API restructuring and av processing (#18)
    * Initalize server template
    
    * Created pickle file for ML access
    
    * Initial code. Testing it functions
    
    * Working predict route for text
    
    * Add requirements.txt and cleanup
    
    * Remove venv
    
    * Version 1 of ML API
    
    * Project restructuring
    
    * Import FER error
    
    * WIP cv
    
    * ML additions WIP
    
    * Introduce new API structure as well as audio extraction capabilities
    
    Former-commit-id: ddcd46371269d8b3615994733940892dda4dbde3

---
+++ ml-api/models/models.py
@@ -0,0 +1,22 @@
+import pickle
+# from fer import Video, FER
+
+TEXT_MODEL = pickle.load(open("models/text_model.pkl", "rb"))
+TFIDF_MODEL = pickle.load(open("models/tfidf_model.pkl", "rb"))
+
+# def detect_emotions(videofile_path):
+#     face_detection = FER(mtcnn=True)
+#     input_video = Video(videofile_path)
+#     processed_data = input_video.analyze(face_detection, display=False)
+#     vid_df = input_video.to_pandas(processed_data)
+#     vid_df = input_video.get_emotions(vid_df)
+#     sum_emotions = {
+#         'angry': sum(vid_df.angry),
+#         'disgust': sum(vid_df.disgust),
+#         'fear': sum(vid_df.fear),
+#         'happy': sum(vid_df.happy),
+#         'sad': sum(vid_df.sad),
+#         'surprise': sum(vid_df.surprise),
+#         'neutral': sum(vid_df.neutral)
+#     }
+#     return sum_emotions
