b8a39303aecba706f3823f39e692316690c5dbd7
smax253 <smax253@gmail.com>
2022-04-02 15:05:49 -0400

    Styled table, added links and sorting

--- digital-coach-app/components/molecules/PastInterviewTable.tsx
+++ digital-coach-app/components/molecules/PastInterviewTable.tsx
@@ -1,7 +1,11 @@
 import usePastInterviewQuery from "@App/lib/interview/usePastInterviewQuery";
 import { Timestamp } from "@firebase/firestore";
 import { PropsWithoutRef, useMemo } from "react";
-import { useTable } from "react-table";
+import { useSortBy, useTable } from "react-table";
+import styles from "@App/components/molecules/PastInterviewTable.module.scss";
+import { LinearProgress } from "@mui/material";
+import { ExpandMore, UnfoldMore, ExpandLess } from "@mui/icons-material";
+import Link from "next/link";
 interface Props
   extends React.DetailedHTMLProps<
     React.HTMLAttributes<HTMLElement>,
@@ -9,16 +13,16 @@ interface Props
   > {
   userId: string;
 }
-
 interface TableProps
   extends React.DetailedHTMLProps<
     React.HTMLAttributes<HTMLElement>,
     HTMLElement
   > {
   data: {
+    interviewId: string;
     interviewName: string;
-    date: string;
-    averageScore: number;
+    date: Date;
+    averageScore: number | undefined;
     completionPct: number;
   }[];
 }
@@ -28,32 +32,81 @@ function TableBody(props: PropsWithoutRef<TableProps>) {
   const columns = useMemo(
     () => [
       { accessor: "interviewName", Header: "Name" },
-      { accessor: "date", Header: "Date" },
-      { accessor: "completionPct", Header: "Progress" },
-      { accessor: "averageScore", Header: "Score" },
+      {
+        accessor: "date",
+        Header: "Date",
+        Cell: (props: { value: Date }) => props.value.toString(),
+      },
+      {
+        accessor: "completionPct",
+        Header: "Progress",
+        Cell: (props: { value: number | undefined }) => (
+          <LinearProgress
+            className={styles.ProgressBar}
+            variant="determinate"
+            value={props.value ? props.value * 100 : 0}
+          />
+        ),
+      },
+      {
+        accessor: "averageScore",
+        Header: "Score",
+        Cell: (props: { value: number | undefined }) =>
+          props.value ? (props.value * 100).toFixed(1) + "%" : "--",
+      },
     ],
     []
   );
+
   const tableData = useMemo(() => data, [data]);
-  const { getTableBodyProps, getTableProps, rows, prepareRow, headers } =
-    useTable(
-      //@ts-ignore
-      { columns, data: tableData }
-    );
+  const {
+    getTableBodyProps,
+    getTableProps,
+    rows,
+    prepareRow,
+    headers,
+    headerGroups,
+  } = useTable(
+    //@ts-ignore
+    { columns, data: tableData },
+    useSortBy
+  );
 
   return (
-    <table {...getTableProps()}>
-      <thead>
-        <tr>
-          {headers.map((header) => {
-            const { key, ...restOfHeaderProps } = header.getHeaderProps();
-            return (
-              <th key={key} {...restOfHeaderProps}>
-                {header.render("Header")}
-              </th>
-            );
-          })}
-        </tr>
+    <table {...getTableProps()} className={styles.PastInterviewTable}>
+      <thead className={styles.TableHeader}>
+        {headerGroups.map((headerGroup) => {
+          const { key, ...restOfHeaderGroupProps } =
+            headerGroup.getHeaderGroupProps();
+          return (
+            <tr {...restOfHeaderGroupProps} key={key}>
+              {headerGroup.headers.map((header) => {
+                const { ...restOfHeaderProps } = header.getSortByToggleProps();
+                const { key } = header.getHeaderProps();
+                return (
+                  <th
+                    key={key}
+                    {...restOfHeaderProps}
+                    className={styles.TableLabel}
+                  >
+                    <div>
+                      {header.render("Header")}
+                      {header.isSorted ? (
+                        header.isSortedDesc ? (
+                          <ExpandMore />
+                        ) : (
+                          <ExpandLess />
+                        )
+                      ) : (
+                        <UnfoldMore />
+                      )}
+                    </div>
+                  </th>
+                );
+              })}
+            </tr>
+          );
+        })}
       </thead>
 
       <tbody {...getTableBodyProps()}>
@@ -61,16 +114,19 @@ function TableBody(props: PropsWithoutRef<TableProps>) {
           prepareRow(row);
           const { key: rowKey, ...restOfRowProps } = row.getRowProps();
           return (
-            <tr key={rowKey} {...restOfRowProps}>
-              {row.cells.map((cell) => {
-                  const {key: cellKey, ...restOfCellProps} = cell.getCellProps();
-                return (
-                  <td key={cellKey} {...restOfCellProps}>
-                    {cell.render("Cell")}
-                  </td>
-                );
-              })}
-            </tr>
+            <Link key={rowKey} href={`past/${data[i].interviewId}`} passHref>
+              <tr {...restOfRowProps} className={styles.InterviewRow}>
+                {row.cells.map((cell) => {
+                  const { key: cellKey, ...restOfCellProps } =
+                    cell.getCellProps();
+                  return (
+                    <td key={cellKey} {...restOfCellProps}>
+                      {cell.render("Cell")}
+                    </td>
+                  );
+                })}
+              </tr>
+            </Link>
           );
         })}
       </tbody>
