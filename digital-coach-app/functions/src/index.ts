31a0be5873f6580aa0f5c7b9c03c9a7f1013b8c6
smax253 <smax253@gmail.com>
2022-04-11 21:31:46 -0400

    Added firebase functions hook to submit to ML api on answer upload

--- digital-coach-app/functions/src/index.ts
+++ digital-coach-app/functions/src/index.ts
@@ -1,9 +1,32 @@
+import "./firebase.config";
 import * as functions from "firebase-functions";
+import {getApp} from "firebase/app";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+} from "firebase/storage";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import {default as axios} from "axios";
+
+const app = getApp();
+
+export const answerUpload = functions.firestore.document("users/{userId}/interviews/{interviewId}/interviewQuestions/{questionId}/answers/{answerId}").onCreate(async (snapshot, context) => {
+  const data = snapshot.data();
+
+  const videoRefUrl = data.videoUrl;
+  const videoRef = ref(getStorage(app), videoRefUrl);
+  const videoDownloadUrl = await (await getDownloadURL(videoRef)).toString();
+  if (!process.env.ML_API_URL) {
+    functions.logger.log("ML_API_URL is not set", videoDownloadUrl);
+  } else {
+    try {
+      const MLApiResponse = await axios.post(process.env.ML_API_URL, {
+        videoUrl: videoDownloadUrl,
+      } );
+      snapshot.ref.update({...(MLApiResponse.data)});
+    } catch (error) {
+      functions.logger.error(error);
+    }
+  }
+});
