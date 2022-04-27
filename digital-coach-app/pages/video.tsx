c8e364b81561b637bb8f9baacab5ea4fcc5d2fbf
Michael McCreesh <mmccree1@stevens.edu>
2022-04-27 10:23:21 -0400

    styling on the video recoding page and past interview page and fixed the profile pic bug

--- digital-coach-app/pages/video.tsx
+++ digital-coach-app/pages/video.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from "react";
 import { useReactMediaRecorder } from "react-media-recorder";
+import styles from "@App/styles/VideoPage.module.scss";
 
 export default function VideoPage() {
   const videoRef = useRef<HTMLVideoElement>(null);
@@ -13,11 +14,16 @@ export default function VideoPage() {
 
   return (
     <div>
-      <p>{status}</p>
-      <button onClick={startRecording}>Start Recording</button>
-      <button onClick={stopRecording}>Stop Recording</button>
-      <video src={mediaBlobUrl!} controls autoPlay loop />
-      <video ref={videoRef} controls autoPlay />
+      <p className = {styles.thing}>{status}</p>
+      <div className={styles.videoBox}>
+        <video src={mediaBlobUrl!} controls autoPlay loop />
+        <video ref={videoRef} controls autoPlay />
+      </div>
+      <div className={styles.buttonBox}>
+        <button onClick={startRecording}>Start Recording</button>
+        <button onClick={stopRecording}>Stop Recording</button>
+      </div>
+      
     </div>
   );
 }
