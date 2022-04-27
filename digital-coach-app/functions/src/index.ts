730af941adc9d493d450aad9f853987d3a63b161
smax253 <smax253@gmail.com>
2022-04-27 17:41:13 -0400

    Updated firebase functions to open API endpoint

--- digital-coach-app/functions/src/index.ts
+++ digital-coach-app/functions/src/index.ts
@@ -7,13 +7,32 @@ import {
   ref,
 } from "firebase/storage";
 
+import { getDoc, getFirestore, doc, updateDoc } from "firebase/firestore"
+
 import {default as axios} from "axios";
 
 const app = getApp();
 
+export const answerReceive = functions.https.onRequest(async (req, res) => {
+    const {
+      evaluation, userId, interviewId, questionId, answerId
+    } = req.body;
+    if(!evaluation || !userId || !interviewId || !questionId || !answerId) {
+      res.status(400).send("Bad request");
+    }
+    const firestore = getFirestore();
+    const answerRef = doc(firestore, `users/${userId}/interviews/${interviewId}/questions/${questionId}/answers/${answerId}`);
+    const answer = await getDoc(answerRef);
+    if(!answer.exists) {
+      res.status(400).send("Resource not found");
+    }
+    await updateDoc(answerRef, {evaluation});
+    res.status(200).send("Success");
+});
+
 export const answerUpload = functions.firestore.document("users/{userId}/interviews/{interviewId}/interviewQuestions/{questionId}/answers/{answerId}").onCreate(async (snapshot, context) => {
   const data = snapshot.data();
-
+  const {userId, interviewId, questionId, answerId} = context.params;
   const videoRefUrl = data.videoUrl;
   const videoRef = ref(getStorage(app), videoRefUrl);
   const videoDownloadUrl = await (await getDownloadURL(videoRef)).toString();
@@ -23,8 +42,9 @@ export const answerUpload = functions.firestore.document("users/{userId}/intervi
     try {
       const MLApiResponse = await axios.post(process.env.ML_API_URL, {
         videoUrl: videoDownloadUrl,
+        userId, interviewId, questionId, answerId,
       } );
-      snapshot.ref.update({...(MLApiResponse.data)});
+      functions.logger.log("MLApiResponse sent: ", MLApiResponse);
     } catch (error) {
       functions.logger.error(error);
     }
