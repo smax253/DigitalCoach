0d7857e9a810c2606cd5fabfe7f147071898c84f
Michael McCreesh <mmccree1@stevens.edu>
2022-04-28 19:19:04 -0400

    styling for past interviews

--- digital-coach-app/components/atoms/PastInterviewAnswer/PastInterviewAnswerSentiment.tsx
+++ digital-coach-app/components/atoms/PastInterviewAnswer/PastInterviewAnswerSentiment.tsx
@@ -2,6 +2,7 @@ import {AudioSentiments} from "@App/lib/answer/model"
 import fixCapitalization from "@App/util/fixCapitalization";
 import Mic from "@mui/icons-material/Mic";
 import Card from "../Card";
+import styles from "@App/components/molecules/PastInterviewAnswerPlayer.module.scss"
 
 interface Props
   extends React.DetailedHTMLProps<
@@ -10,9 +11,14 @@ interface Props
   > {
   sentiment: AudioSentiments;
 }
+const sentimentStyleClass={
+  "POSITIVE": styles.positiveSentiment,
+  "NEGATIVE": styles.negativeSentiment,
+  "NEUTRAL": styles.neutralSentiment 
+}
 
 export default function PastInterviewAnswerSentiment(props: Props){
-    return <Card>
+    return <Card className={sentimentStyleClass[props.sentiment]}>
       <h3>Most Common Audio Sentiment</h3>
       <div><Mic/>{fixCapitalization(props.sentiment)}</div>
       </Card>
