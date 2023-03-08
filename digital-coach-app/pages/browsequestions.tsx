d264e3db5e2022aacd193c25a443cbfa08280a35
jkronyak <56709613+jkronyak@users.noreply.github.com>
2023-03-08 15:11:25 -0500

    Added basic elements to Browse Questions

--- digital-coach-app/pages/browsequestions.tsx
+++ digital-coach-app/pages/browsequestions.tsx
@@ -1,9 +1,12 @@
 import useAuthContext from "@App/lib/auth/AuthContext";
 import AuthGuard from "@App/lib/auth/AuthGuard";
 
+import styles from "@App/styles/BrowseQuestionsPage.module.scss";
+
+
 // import { DataGrid } from '@mui/x-data-grid';
 
-import { TextField } from "@App/components/molecules/TextField";
+// import { TextField } from "@App/components/molecules/TextField";
 
 import { 
 	List,
@@ -12,9 +15,14 @@ import {
 	ListItemIcon,
 	IconButton,
 	Divider,
+	TextField,
+	Box,
+	Checkbox
 } from "@mui/material";
 
 import AddIcon from '@mui/icons-material/Add';
+
+
 
 const sampleData = [
 	{
@@ -53,33 +61,81 @@ const sampleData = [
 		},
 ];
 
+const sampleSubjects = [ 
+	"Business Accounting and Analytics",
+	"Business Management",
+	"Business Marketing",
+	"Business Operations",
+	"Business Strategy",
+	"Business Technology",
+	"Data Science",
+	"Finance",
+	"Human Resources",
+	"Information Technology",
+	"Law",
+]
+
 function BrowseQuestionsPage() { 
 	const { currentUser } = useAuthContext();
 
 	return (
-		<div>
+		<div className={styles.BrowseQuestionsPage}>
 			<h1>Browse Questions</h1>
-			<TextField title="Search"></TextField>
-			<List sx={{ width: '100%', bgcolor: 'background.paper' }}>
-				{
-					sampleData.map((question) => (
-						<div>
-							<ListItem>
-								<ListItemIcon>
-									<IconButton>
-										<AddIcon />
-									</IconButton>
-								</ListItemIcon>
-								<ListItemText 
-									primary={question.question}
-									secondary={[question.subject, question.type, question.position].join(" | ")}
-									/>
-							</ListItem>
-							<Divider />
-						</div>
-					))
-				}
-			</List>
+			<TextField label='Search Questions'></TextField>
+			<Box sx={{
+				maxWidth: '80%',
+				display: 'flex', 
+				flexDirection: 'row',   
+				verticalAlign: 'top',
+				mt: 2
+				}}
+			>
+				<List sx={{ bgcolor: 'background.paper' }}>
+					{
+						sampleData.map((question) => (
+							<div>
+								<ListItem>
+									<ListItemIcon>
+										<IconButton>
+											<AddIcon />
+										</IconButton>
+									</ListItemIcon>
+									<ListItemText 
+										primary={question.question}
+										secondary={[question.subject, question.type, question.position].join(" | ")}
+										/>
+								</ListItem>
+								<Divider />
+							</div>
+						))
+					}
+				</List>
+				<div className='filter-div'>
+					<h2>Filters</h2>
+					<label htmlFor='popularity-check-box'>Sort By Popularity</label>
+					<input type="checkbox" id='popularity-check-box'></input>
+					<label htmlFor='subject-select'>Subject</label>
+					<select id='subject-select'>
+						{
+							sampleSubjects.map((subject) => (
+								<option value={subject}>{subject}</option>
+							))
+						}
+					</select>
+					<label htmlFor='type-select'>Type</label>
+					<select id='type-select'>
+						<option value='Technical'>Technical</option>
+						<option value='Behavioral'>Behavioral</option>
+					</select>
+					<label htmlFor='experience-level-select'>Experience Level</label>
+					<select id='experience-level-select'>
+						<option value='Entry'>Entry</option>
+						<option value='Mid'>Mid</option>
+						<option value='Senior'>Senior</option>
+					</select>
+
+				</div>
+			</Box>
 		</div>
 	);
 }
